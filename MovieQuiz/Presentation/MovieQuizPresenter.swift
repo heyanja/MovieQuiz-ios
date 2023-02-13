091e2b78bb323f786d49d0c7676688a07a97c827
Anna <119071299+heyanja@users.noreply.github.com>
2023-02-13 16:08:24 -0500

    Спринт 7 Рефакторинг Шаг3

--- MovieQuiz/Presentation/MovieQuizPresenter.swift
+++ MovieQuiz/Presentation/MovieQuizPresenter.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 final class MovieQuizPresenter {
+    private let questionsAmount: Int = 10
+    private var currentQuestionIndex: Int = 0
+    
     func convert(model: QuizQuestion) -> QuizStepViewModel {
         return QuizStepViewModel(
             image: UIImage(data: model.image) ?? UIImage(),
