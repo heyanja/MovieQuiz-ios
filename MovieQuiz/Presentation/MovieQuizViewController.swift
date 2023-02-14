fe068d6da4bec3cbebe42a2edcbf959cc3cdcb42
Anna <119071299+heyanja@users.noreply.github.com>
2023-02-14 00:32:10 -0500

    Спринт 7 Рефакторинг 1. Перенесли логику `yesButtonClicked(_ sender: UIButton)` в `presenter`; 2. Перенесли логику `noButtonClicked(_ sender: UIButton)` в `presenter`; 3. Применили DRY: устранили дублирование, возникшее после шагов 1 и 2 — вынесли дублирующийся код в метод `func didAnswer(isYes: Bool)`; 4. Применили DRY: устранили дублирование в использовании `currentQuestion` после шагов 1 и 2; 5. Перенесли метод `showNextQuestionOrResults` в `presenter`; 6. Применили DRY: устранили дублирование `correctAnswers`; 7. Применили DRY: устранили дублирование `questionFactory`; 8. Перенесли логику формирования сообщения с результатами квиза в метод `presenter`'а `makeResultsMessage()`, а также применили DRY для устранения дублирования свойства `statisticService`; 9. Финальный шаг — перенесли в `presenter` асинхронный код (`DispatchQueue.main.asyncAfter`).

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,112 +1,14 @@
-//import UIKit
-//
-//final class MovieQuizViewController: UIViewController, MovieQuizViewControllerProtocol {
-//
-//    // MARK: - Properties
-//
-//    private var alertPresenter: AlertPresenter!
-//    private var presenter: MovieQuizPresenter!
-//    override var preferredStatusBarStyle: UIStatusBarStyle { .lightContent }
-//
-//
-//    @IBOutlet weak var imageView: UIImageView!
-//    @IBOutlet private weak var textLabel: UILabel!
-//    @IBOutlet private weak var counterLabel: UILabel!
-//    @IBOutlet private weak var textOfQuestion: UILabel!
-//    @IBOutlet private weak var questionLabelText: UILabel!
-//    @IBOutlet private weak var indexQuestionText: UILabel!
-//    @IBOutlet private weak var noButton: UIButton!
-//    @IBOutlet private weak var yesButton: UIButton!
-//    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
-//
-//
-//    // MARK: Lifecycle
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//        alertPresenter = AlertPresenter(viewController: self)
-//        presenter = MovieQuizPresenter(viewController: self)
-//
-//        showLoadingIndicator()
-//    }
-//
-//
-//    // MARK: - Actions
-//
-//    @IBAction private func yesButtonClicked(_ sender: UIButton) {
-//        presenter.yesButtonClicked()
-//        blockButton()
-//    }
-//    @IBAction private func noButtonClicked(_ sender: UIButton) {
-//        presenter.noButtonClicked()
-//        blockButton()
-//    }
-//
-//    func unlockButton() {
-//        noButton.isEnabled = true
-//        yesButton.isEnabled = true
-//    }
-//
-//    func blockButton() {
-//        noButton.isEnabled = false
-//        yesButton.isEnabled = false
-//    }
-//
-//    func show(quiz step: QuizStepViewModel) {
-//        UIView.transition(with: imageView,
-//                          duration: 0.3,
-//                          options: .transitionCrossDissolve,
-//                          animations: { self.imageView.image = step.image},
-//                          completion: nil)
-//        imageView.image = step.image
-//        imageView.layer.cornerRadius = 20
-//        imageView.animationDuration = 1
-//        textLabel.text = step.question
-//        counterLabel.text = step.questionNumber
-//    }
-//
-//    func showLoadingIndicator() {
-//        activityIndicator.isHidden = false // говорим, что индикатор загрузки не скрыт
-//        activityIndicator.startAnimating() // включаем анимацию
-//    }
-//    func hideLoadingIndicator() {
-//        activityIndicator.isHidden = true
-//        activityIndicator.stopAnimating()
-//    }
-//
-//    func showNetworkError(message: String) {
-//        hideLoadingIndicator()
-//
-//        let alert = AlertModel(title: "Ошибка", message: message, buttonText: "Попробовать еще раз") { [weak self] in
-//            guard let self = self else { return }
-//            self.presenter.restartGame()
-//        }
-//        alertPresenter?.showQuizResult(model: alert)
-//    }
-//
-//
-//    func highlightImageBorder(isCorrect: Bool) {
-//        imageView.layer.masksToBounds = true
-//        imageView.layer.borderWidth = 8
-//        imageView.layer.cornerRadius = 20
-//        if isCorrect {
-//            imageView.layer.borderColor = UIColor.ypGreen.cgColor
-//        } else {
-//            imageView.layer.borderColor = UIColor.ypRed.cgColor
-//        }
-//    }
-//}
-//
-//
 import UIKit
 
 final class MovieQuizViewController: UIViewController, MovieQuizViewControllerProtocol {
-   
-    private var presenter: MovieQuizPresenter!
+
+    // MARK: - Properties
+
     private var alertPresenter: AlertPresenter!
+    private var presenter: MovieQuizPresenter!
     override var preferredStatusBarStyle: UIStatusBarStyle { .lightContent }
-    
+
+
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet private weak var textLabel: UILabel!
     @IBOutlet private weak var counterLabel: UILabel!
@@ -116,48 +18,66 @@ final class MovieQuizViewController: UIViewController, MovieQuizViewControllerPr
     @IBOutlet private weak var noButton: UIButton!
     @IBOutlet private weak var yesButton: UIButton!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
-    
+
+
+    // MARK: Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         alertPresenter = AlertPresenter(viewController: self)
         presenter = MovieQuizPresenter(viewController: self)
+
         showLoadingIndicator()
     }
-    
-    @IBAction private func noButtonClicked(_ sender: UIButton) {
-        presenter.noButtonClicked()
-        blockButton()
-    }
-    
+
+
+    // MARK: - Actions
+
     @IBAction private func yesButtonClicked(_ sender: UIButton) {
         presenter.yesButtonClicked()
         blockButton()
     }
-    
+    @IBAction private func noButtonClicked(_ sender: UIButton) {
+        presenter.noButtonClicked()
+        blockButton()
+    }
+
     func unlockButton() {
         noButton.isEnabled = true
         yesButton.isEnabled = true
     }
-    
+
     func blockButton() {
         noButton.isEnabled = false
         yesButton.isEnabled = false
     }
-    
-    func showLoadingIndicator() {
-        activityIndicator.isHidden = false
-        activityIndicator.startAnimating()
+
+    func show(quiz step: QuizStepViewModel) {
+        UIView.transition(with: imageView,
+                          duration: 0.3,
+                          options: .transitionCrossDissolve,
+                          animations: { self.imageView.image = step.image},
+                          completion: nil)
+        imageView.image = step.image
+        imageView.layer.cornerRadius = 20
+        imageView.animationDuration = 1
+        textLabel.text = step.question
+        counterLabel.text = step.questionNumber
     }
-    
+
+    func showLoadingIndicator() {
+        activityIndicator.isHidden = false // говорим, что индикатор загрузки не скрыт
+        activityIndicator.startAnimating() // включаем анимацию
+    }
     func hideLoadingIndicator() {
         activityIndicator.isHidden = true
         activityIndicator.stopAnimating()
     }
-    
+
     func showNetworkError(message: String) {
         hideLoadingIndicator()
-        
+
         let alert = AlertModel(title: "Ошибка", message: message, buttonText: "Попробовать еще раз") { [weak self] in
             guard let self = self else { return }
             self.presenter.restartGame()
@@ -165,19 +85,7 @@ final class MovieQuizViewController: UIViewController, MovieQuizViewControllerPr
         alertPresenter?.showQuizResult(model: alert)
     }
 
-    func show(quiz step: QuizStepViewModel) {
-        UIView.transition(with: imageView,
-                                  duration: 0.3,
-                                  options: .transitionCrossDissolve,
-                                  animations: { self.imageView.image = step.image},
-                                  completion: nil)
-        imageView.image = step.image
-        imageView.layer.cornerRadius = 20
-        imageView.animationDuration = 1
-        textLabel.text = step.question
-        counterLabel.text = step.questionNumber
-    }
-    
+
     func highlightImageBorder(isCorrect: Bool) {
         imageView.layer.masksToBounds = true
         imageView.layer.borderWidth = 8
